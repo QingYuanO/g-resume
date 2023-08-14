dce1eea41603580a4f8769b630bc3bbb9cb4f46d
chenhu <chenhu@reedsec.com>
2023-08-14 10:39:29 +0800

    使用vite

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import CVForm, { FormValues } from '@/components/CVForm';
+import MyDocument from '@/components/CVTemp/MyDocument';
+import { useState } from 'react';
+
+export default function Home() {
+  const [cvData, setCvData] = useState<FormValues>({ name: '', job: '' });
+  const handleGeneratePdf = (params: FormValues) => {
+    setCvData(params);
+  };
+  return (
+    <main className='flex container gap-x-4'>
+      <div className='flex-1'>
+        <CVForm onGeneratePdf={handleGeneratePdf} />
+      </div>
+      <div className='flex-1'>
+        <MyDocument cvData={cvData} />
+      </div>
+    </main>
+  );
+}
---
+++ src/components/CVForm/index.tsx
@@ -0,0 +1,62 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  name: z.string().nonempty('不能为空'),
+  job: z.string().nonempty('不能为空'),
+});
+export type FormValues = z.infer<typeof formSchema>;
+
+export default function CVForm(props: { onGeneratePdf?: (data: FormValues) => void }) {
+  const { onGeneratePdf } = props;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    onGeneratePdf?.(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='gap-x-8 gap-y-2 flex flex-wrap'>
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem className='basis-[calc(50%-4rem/2)]'>
+              <FormLabel>姓名</FormLabel>
+              <FormControl>
+                <Input placeholder='请输入姓名' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='job'
+          render={({ field }) => (
+            <FormItem className='basis-[calc(50%-4rem/2)]'>
+              <FormLabel>职位名称</FormLabel>
+              <FormControl>
+                <Input placeholder='请输入职位名称' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className='basis-full'></div>
+        <Button type='submit'>生成基本信息</Button>
+      </form>
+    </Form>
+  );
+}
