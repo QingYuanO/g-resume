a0bd08008e5fdc00501db1525409f6c328b0418a
qingyuano <211720950@qq.com>
2023-09-05 22:50:56 +0800

    update : 修改网站名称、图标

--- src/components/CVTemp/PDFDocument.tsx
+++ src/components/CVTemp/PDFDocument.tsx
@@ -1,23 +1,22 @@
 "use client";
 /* eslint-disable jsx-a11y/alt-text */
-import { StyleSheet, Font,PDFViewer } from "@react-pdf/renderer";
+import { StyleSheet, Font, PDFViewer } from "@react-pdf/renderer";
 import dynamic from "next/dynamic";
 import useResumeStore from "@/store/resume";
 import T1 from "./T1";
+
 
 Font.register({
   family: "LXGWFasmartGothic",
   src: "/font/LXGWFasmartGothic.ttf",
 });
 
-
-
 const PDFDocument = () => {
   const resume = useResumeStore((state) => state.data);
   return (
-    <PDFViewer className="h-screen w-full">
+    <DynamicPDFViewer className="h-screen w-full">
       <T1 resume={resume} />
-    </PDFViewer>
+    </DynamicPDFViewer>
   );
 };
 
@@ -27,5 +26,6 @@ const DynamicPDFViewer = dynamic(
   () => import("@react-pdf/renderer").then((module) => module.PDFViewer),
   {
     ssr: false,
+    loading: () => <div>加载中...</div>,
   },
 );
