f02e7583760f44eb120fb27bd557162f3104ab96
chenhu <chenhu@reedsec.com>
2023-09-11 16:18:51 +0800

    update : 暗夜模式修改

--- src/components/ThemeProvider/index.tsx
+++ src/components/ThemeProvider/index.tsx
@@ -5,6 +5,6 @@ import React, { ReactNode } from "react";
 export default function ThemeProvider({ children }: { children?: ReactNode }) {
   const theme = useThemeStore((state) => state.theme);
   return (
-    <div className={`${theme} bg-background text-foreground`}>{children}</div>
+    <div className={`${theme} bg-background text-foreground w-full h-full`}>{children}</div>
   );
 }
