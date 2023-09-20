d38e85d1ca9efd9777fa81e482f0866afb5da9d9
chenhu <chenhu@reedsec.com>
2023-09-20 14:55:38 +0800

    ui ：修改主页背景宽度

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       {/* <div className="pointer-events-none fixed inset-0 select-none bg-[url('/grid-black.svg')] bg-top bg-repeat dark:bg-[url('/grid.svg')]" /> */}
       {/* <span className="pointer-events-none fixed top-0 block h-[800px] w-full select-none bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(5,5,5,0.045)_0%,rgba(0,0,0,0)_100%)] dark:bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(255,255,255,0.09)_0%,rgba(255,255,255,0)_100%)]" /> */}
       <div className="fixed inset-0 flex justify-center px-8 md:p-0">
-        <div className="flex w-full max-w-5xl md:px-8">
+        <div className="flex w-full max-w-3xl md:px-8">
           <div className="w-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/80 dark:ring-zinc-400/20" />
         </div>
       </div>
