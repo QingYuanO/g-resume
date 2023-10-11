49fc6aecfa08429ddb4d28e42396377e99f885e1
chenhu <chenhu@reedsec.com>
2023-10-11 09:17:34 +0800

    update : 修改切换暗夜模式按钮

--- src/components/ThemeProvider/ThemeToggleBtn.tsx
+++ src/components/ThemeProvider/ThemeToggleBtn.tsx
@@ -1,19 +1,28 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 
 export default function ThemeToggleBtn() {
-  const { setTheme,theme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, theme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  
   return (
     <Button
       variant="outline"
       className=" fixed bottom-32 right-4 z-50 flex-shrink-0 rounded-full opacity-90 shadow md:relative md:bottom-0 md:right-0"
-      onClick={() => setTheme(theme === "light" ? 'dark' : 'light')}
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       size="icon"
     >
-      {theme === "light" ? <Moon /> : <Sun />}
+      {theme === "light" ? <Sun /> : <Moon />}
     </Button>
   );
 }
